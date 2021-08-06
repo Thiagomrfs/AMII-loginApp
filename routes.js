e4ac4e7bd3c26b96b0ea264a953e77ac5bb9334a
Thiago <thiagomarbsn@gmail.com>
2021-08-06 14:09:04 -0300

    routes

--- routes.js
+++ routes.js
@@ -4,8 +4,24 @@ const router = express.Router();
 let db = require("./db");
 
 router.get("/", (req, res) => {
-    res.render("pages/home");
+    // res.render("pages/home");
     res.send("pagina de teste");
+});
+
+router.get("/about",(req,res)=>{
+    res.send("Sobre");
+});
+
+router.get("/curriculo",(req,res)=>{
+    res.send("Meu currículo");
+});
+
+router.get("/cadastro/insert",(req,res)=>{
+    res.send("inserir um usuario");
+});
+
+router.get("/cadastro/list",(req,res)=>{
+    res.send("listar de usuarios cadastrado");
 });
 
 module.exports = router;
